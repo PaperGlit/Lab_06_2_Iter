c161f36506828134441ba17f0c4fc5973efd4102
Glit <vpad.lazar.04@gmail.com>
2022-11-13 18:00:01 +0200

    Add project files.

---
+++ Lab_06_2_Iter/Lab_06_2_Iter.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <time.h>
+#include <windows.h>
+
+using namespace std;
+
+void Create(int* a, const int size, const int Low, const int High)
+{
+	for (int i = 0; i < size; i++)
+		a[i] = Low + rand() % (High - Low + 1);
+}
+
+void Print(int* a, const int size)
+{
+	cout << "Значення масиву : ";
+	for (int i = 0; i < size; i++)
+		cout << a[i] << " ";
+	cout << endl;
+}
+
+int Max(int* a, const int size)
+{
+	int max = 0;
+	for (int i = 0; i < size; i++)
+		if (a[i] % 2 != 0)
+		{
+			max = a[i];
+			break;
+		}
+	for (int i = 0; i < size; i++)
+		if (a[i] % 2 != 0 && a[i] > max)
+			max = a[i];
+	return max;
+}
+
+int iMax(int* a, const int size)
+{
+	int max = 0, imax = 0;
+	for (int i = 0; i < size; i++)
+		if (a[i] % 2 != 0)
+		{
+			max = a[i];
+			imax = i;
+			break;
+		}
+	for (int i = 0; i < size; i++)
+		if (a[i] % 2 != 0 && a[i] > max)
+		{
+			max = a[i];
+			imax = i;
+		}
+	return imax;
+}
+
+int* Swap(int* a, const int size)
+{
+	int* c = a;
+	int imax = iMax(a, size), temp = c[size - 1];
+	c[size - 1] = c[imax];
+	c[imax] = temp;
+	return c;
+}
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	srand((unsigned)time(NULL));
+
+	int n, Low, High;
+
+	cout << "n = "; cin >> n;
+	cout << "min = "; cin >> Low;
+	cout << "max = "; cin >> High;
+
+	int* c = new int[n];
+
+	Create(c, n, Low, High);
+	Print(c, n);
+	cout << "Значення, яке підходить вимогам : " << Max(c, n) << endl;
+	Print(Swap(c, n), n);
+
+	return 0;
+}
---
+++ UnitTest_Lab_06_2_Iter/UnitTest_Lab_06_2_Iter.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_06_2_Iter/Lab_06_2_Iter.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+void Test(int* a, const int size)
+{
+	int max = 0, imax = 0;
+	int* c = a;
+	for (int i = 0; i < size; i++)
+		if (a[i] % 2 != 0)
+		{
+			max = a[i];
+			imax = i;
+			break;
+		}
+	for (int i = 1; i < size; i++)
+		if (a[i] % 2 != 0 && a[i] > max)
+		{
+			max = a[i];
+			imax = i;
+		}
+	Assert::AreEqual(max, Max(a, size));
+	Assert::AreEqual(imax, iMax(a, size));
+	int temp = a[size - 1];
+	a[size - 1] = a[imax];
+	a[imax] = temp;
+	c = Swap(c, size);
+	for (int i = 0; i < size; i++)
+		Assert::AreEqual(a[i], c[i]);
+}
+
+namespace UnitTestLab062Iter
+{
+	TEST_CLASS(UnitTestLab062Iter)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 10;
+			int c[n];
+			int Low = -10, High = 10;
+			Create(c, n, Low, High);
+			Test(c, n);
+		}
+	};
+}
